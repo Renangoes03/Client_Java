5e4e95b9fd1c8e1fa3da6d33818e786bdeee593f
Renangoes03 <renangoessantos9@gmail.com>
2023-05-30 11:02:01 -0300

    Chamada concluida

--- src/main/java/com/fatec/client/resources/ClientController.java
+++ src/main/java/com/fatec/client/resources/ClientController.java
@@ -1,32 +1,30 @@
 package com.fatec.client.resources;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatec.client.entities.Client;
+import com.fatec.client.services.ClientService;
 
 @RestController
 public class ClientController {
+    @Autowired
+    private ClientService clientService;
 
-    @GetMapping("client")
-    public Client getClient() {
-        Client c1 = new Client(1,"joao@gmail.com", "Joao Silva");
-        return c1;
+    @GetMapping("clients/{id}")
+    public Client getClient(@PathVariable int id) {
+        return clientService.getClientById(id);
+      
     }
 
     @GetMapping("clients")
     public List<Client> getClients() {
-        List <Client>clients = new ArrayList<Client>();
-        Client c1 = new Client(1,"joao@gmail.com", "Joao Silva");
-        Client c2 = new Client(2,"maria@gmail.com", "Maria");
-
-        clients.add(c1);
-        clients.add(c2);
-      
-        return clients;
+        return clientService.getClients();
 
     }
 
